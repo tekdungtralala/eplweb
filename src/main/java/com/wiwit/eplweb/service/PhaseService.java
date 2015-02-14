9dbd96ae674d37911357992c35497dca97d13422
tekdungtralala <tekdung554@gmail.com>
2015-02-15 00:06:07 +0700

    clean up code

--- src/main/java/com/wiwit/eplweb/service/PhaseService.java
+++ src/main/java/com/wiwit/eplweb/service/PhaseService.java
@@ -2,7 +2,6 @@ package com.wiwit.eplweb.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.wiwit.eplweb.dao.PhaseDAO;
