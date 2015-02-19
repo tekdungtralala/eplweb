5a75a660d79b444e1f69525af5474a0e8aa28c1a
tekdungtralala <tekdung554@gmail.com>
2015-02-19 23:22:44 +0700

    Create template page /team/:id/:name

--- src/main/webapp/app/team/team.js
+++ src/main/webapp/app/team/team.js
@@ -14,7 +14,11 @@
         }
 
         vm.containerLbl = ['Overview', 'Squad', 'Statistic', 'Map', 'Video'];
-        vm.container = [false, true, true, true, true]; // true to hide
+
+        // Container below carousel/slideshow, false for active
+        vm.container = [true, true, false, true, true]; 
+
+        vm.statContainerLbl = ['Played', 'Won', 'Drawn', 'Lost', 'Points'];
 
         $('[data-toggle="tooltip"]').tooltip();
 
@@ -28,7 +32,58 @@
             // });
         }
 
-        vm.selectElement = function(index){
+        initChart(
+[ {
+    "data" : [ 53.0, 16.0, 2.0, 5.0, 52.0, 20.0 ],
+    "name" : "Chelsea"
+  }, {
+    "data" : [ 31.0, 8.0, 8.0, 6.0, 29.0, 29.0 ],
+    "name" : "Other Team"
+  } ]
+            ,
+            [ "Points", "Win Rate", "Win Lose", "Win Draw", "Goal Scored", "Goal Against" ]
+            );
+        function initChart(series, categories){
+            $('#epl-chart-container').highcharts({
+                series: series,
+                xAxis: {
+                    categories: categories
+                },
+                chart: {
+                    type: 'column'
+                },
+                title: {
+                    text: '',
+                    style: {
+                        'display': 'none'
+                    }
+                },
+                yAxis: {
+                    min: 0,
+                    title: {
+                        text: 'Rainfall (mm)'
+                    }
+                },
+                tooltip: {
+                    headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                    pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                        '<td style="padding:0"><b>{point.y:.1f}</b></td></tr>',
+                    footerFormat: '</table>',
+                    shared: true,
+                    useHTML: true
+                },
+                plotOptions: {
+                    column: {
+                        pointPadding: 0.2,
+                        borderWidth: 0
+                    }
+                },
+                exporting: { enabled: false }
+            });
+        }
+
+        // ngClick
+        vm.selectContainer = function(index){
             _.each(vm.container, function(i, contIndex){
                 vm.container[contIndex] = true;
             });
